491bb8172ff213ad7b3d17d43d52e15204349df7
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-24 14:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object);
-  newObject.key = value;
+  newObject[key] = value;
   object.push(newObject);
   object;
 }
