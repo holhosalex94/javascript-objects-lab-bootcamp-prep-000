b659242507488063950e87019f050d24be150e13
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-24 14:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var recipes = {'recipe': 'object'};
 function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object);
-  newObject
+  newObject.key = value;
   newObject;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object = Object.assign(key, value);
+  object.key = value;
   object;
 }
